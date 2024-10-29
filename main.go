f475a6a610c91603f173397347c16f9ef9d68ffd
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-10-29 14:27:24 -0400

    feat: add C1 control codes

--- main.go
+++ main.go
@@ -121,6 +121,7 @@ func main() {
 }
 
 var ctrlCodes = map[byte]string{
+	// C0
 	0:  "null",
 	1:  "start of heading",
 	2:  "start of text",
@@ -153,4 +154,38 @@ var ctrlCodes = map[byte]string{
 	29: "group separator",
 	30: "record separator",
 	31: "unit separator",
+
+	// C1
+	0x80: "padding character",
+	0x81: "high octet preset",
+	0x82: "break permitted here",
+	0x83: "no break here",
+	0x84: "index",
+	0x85: "next line",
+	0x86: "start of selected area",
+	0x87: "end of selected area",
+	0x88: "character tabulation set",
+	0x89: "character tabulation with justification",
+	0x8a: "line tabulation set",
+	0x8b: "partial line forward",
+	0x8c: "partial line backward",
+	0x8d: "reverse line feed",
+	0x8e: "single shift 2",
+	0x8f: "single shift 3",
+	0x90: "device control string",
+	0x91: "private use 1",
+	0x92: "private use 2",
+	0x93: "set transmit state",
+	0x94: "cancel character",
+	0x95: "message waiting",
+	0x96: "start of guarded area",
+	0x97: "end of guarded area",
+	0x98: "start of string",
+	0x99: "single graphic character introducer",
+	0x9a: "single character introducer",
+	0x9b: "control sequence introducer",
+	0x9c: "string terminator",
+	0x9d: "operating system command",
+	0x9e: "privacy message",
+	0x9f: "application program command",
 }
